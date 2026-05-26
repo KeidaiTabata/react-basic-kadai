b019439424cca7cd1a9d6aa7ac3686a97f5130a6
KeidaiTabata <keidai.tabata@d2c.co.jp>
2026-05-26 11:14:03 +0900

    一人分の情報だけ渡すように変更

--- react-profile-card/src/ProfileCard.tsx
+++ react-profile-card/src/ProfileCard.tsx
@@ -1,31 +1,19 @@
-import type React from "react";
-
 export type profiles = {
-    name: string,
-    age: number,
-    bio: string
+    name: string;
+    age: number;
+    bio: string;
 };
 
 type props = {
-    count: number,
-    profiles: profiles[],
-    onIncrease: React.MouseEventHandler<HTMLButtonElement>
+    profile: profiles;
+};
 
-}
-
-export function ProfileCard({ count, profiles, onIncrease }: props) {
+export function ProfileCard({ profile }: props) {
     return (
-        <div className="cardWrapper">
-            <ul>
-                {profiles.filter((_, index) => index === count).map(profile => (
-                    <li key={profile.name}>
-                        <h2>{profile.name}</h2>
-                        <p>【年齢】 {profile.age}歳</p>
-                        <p>【自己紹介】{profile.bio}</p>
-                    </li>
-                ))}
-            </ul>
-            <button onClick={onIncrease}>次のプロフィール</button>
-        </div>
+        <li>
+            <h2>{profile.name}</h2>
+            <p>【年齢】 {profile.age}歳</p>
+            <p>【自己紹介】{profile.bio}</p>
+        </li>
     )
 }
